f405c63577e8e02af74b5b2eae76c1529a1dda80
yangdinglou <yangdinglou@gmail.com>
2023-09-12 18:31:47 +0800

    add predicates' examples

--- predicates/new/Reynolds/bktree.pl
+++ predicates/new/Reynolds/bktree.pl
@@ -1,2 +1,2 @@
-bktree(X,S,B,T):- back(X,B), value(X,T), empty(S), !.
-bktree(X,S,B,T):- back(X,B), left(X, L), right(X, R), value(X,T),zero(Zero),one(One),bktree(L,S1,X,Zero), bktree(R,S2,X,One), union(S1,S2,S), !.
+bktree(X,S,B):- back(X,B), empty(S), !.
+bktree(X,S,B):- back(X,B), left(X, L), right(X, R), value(X,T),zero(Zero),one(One),bktree(L,S1,X,Zero), bktree(R,S2,X,One), union(S1,S2,S), !.
