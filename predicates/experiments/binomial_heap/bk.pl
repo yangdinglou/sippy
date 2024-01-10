3b2165b1d32667cd3bb0cf9a3626ec5c7324702b
yangdinglou <yangdinglou@gmail.com>
2024-01-11 01:13:33 +0800

    Update bk.pl

--- predicates/experiments/binomial_heap/bk.pl
+++ predicates/experiments/binomial_heap/bk.pl
@@ -1,7 +1,36 @@
+p(X, R) :- nullptr(X), zero(R).
+p(X, R) :- key(X, V), child(X, C), p(C, R1), my_succ(R1, R), sibling(X, X1), p(X1, R2), anynumber(R2), anynumber(V).
+
+nullptr(null).
+anynumber(_).
+
+key(x0,67).
+key(x1,1).
+key(x2,4).
+key(x3,7).
+key(x4,4).
+
+child(x0,null).
+child(x1,x2).
+child(x2,x3).
+child(x3,null).
+child(x4,null).
+
+sibling(x0,x1).
+sibling(x1,null).
+sibling(x2,x4).
+sibling(x3,null).
+sibling(x4,null).
+
+my_succ(A, B) :-
+    B is A + 1.
+
+q(X, S) :- nullptr(X), empty(S).
+q(X, S) :- key(X, V), child(X, C), q(C, S1), sibling(X, X1), q(X1, S2), ord_union(S1,S2,Tmp),insert(Tmp,V,S).
+
 use_module(library(lists)).
 empty([]).
 zero(0).
-nullptr(null).
 
 gt_list(_, []).
 gt_list(A, B) :-
@@ -19,129 +48,6 @@ minnum(A, B, C) :-
 diff_lessthanone(A, B):-
     D is A - B, abs(D, AbsD), AbsD =< 1.
 
-my_succ(A, B) :-
-    B is A + 1.
-
-my_prev(A, B) :-
-    B is A - 1.
 
 insert(A, B, C) :-
     is_ordset(A), ord_union([B], A, C).
-
-anypointer(_).
-anynumber(_).
-
-same_ptr(A, A).
-
-ge(A, B) :-
-    A >= B.
-
-
-value(aa, 10).
-left(aa, bb).
-right(aa, cc).
-height(aa, 2).
-
-value(bb, 5).
-left(bb, null).
-right(bb, null).
-height(bb, 0).
-
-value(cc, 15).
-left(cc, dd).
-right(cc, null).
-height(cc, 1).
-
-value(dd, 12).
-left(dd, null).
-right(dd, null).
-height(dd, 0).
-
-value(aaa, 10).
-left(aaa, bbb).
-right(aaa, ccc).
-height(aaa, 2).
-
-value(bbb, 5).
-left(bbb, null).
-right(bbb, null).
-height(bbb, 0).
-
-value(ccc, 15).
-left(ccc, null).
-right(ccc, ddd).
-height(ccc, 1).
-
-value(ddd, 17).
-left(ddd, null).
-right(ddd, null).
-height(ddd, 0).
-
-value(a, 50).
-left(a, b).
-right(a, i).
-height(a, 3).
-
-value(b, 30).
-left(b, c).
-right(b, f).
-height(b, 2).
-
-value(c, 20).
-left(c, d).
-right(c, e).
-height(c, 1).
-
-value(d, 10).
-left(d, null).
-right(d, null).
-height(d, 0).
-
-value(e, 25).
-left(e, null).
-right(e, null).
-height(e, 0).
-
-value(f, 40).
-left(f, g).
-right(f, h).
-height(f, 1).
-
-value(g, 35).
-left(g, null).
-right(g, null).
-height(g, 0).
-
-value(h, 45).
-left(h, null).
-right(h, null).
-height(h, 0).
-
-value(i, 60).
-left(i, j).
-right(i, null).
-height(i, 1).
-
-value(j, 55).
-left(j, null).
-right(j, null).
-height(j, 0).
-
-
-
-diff_lessthanone(A, B):-
-    D is A - B, abs(D, AbsD), AbsD =< 1.
-
-my_succ(A, B) :-
-    B is A + 1.
-
-my_prev(A, B) :-
-    B is A - 1.
-
-insert(A, B, C) :-
-    is_ordset(A), ord_union([B], A, C).
-
-
-p(X, R) :- key(X, V), child(X, C), p(C, R1), my_succ(R1, R), sibling(X, X1), p(X1, R2), anynumber(R2), anynumber(V).
-
-p(X, S) :- key(X, V), child(X, C), p(C, S1), sibling(X, X1), p(X1, S2),  
